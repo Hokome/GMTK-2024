cf1dc6a2889c72603388e3e99ea44bc610cb2752
Raylock <39481834+RaylockOblivion@users.noreply.github.com>
2024-08-18 22:21:57 +0530

    working_settings_page

--- ui/menus/settings_menu.gd
+++ ui/menus/settings_menu.gd
@@ -2,6 +2,7 @@ extends Control
 class_name SettingsMenu
 
 @onready var res_drop_down: OptionButton = $center_container/entire_container/v_box_container/resolution_container/res_drop_down
+@onready var window_drop_down: OptionButton = $center_container/entire_container/v_box_container/resolution_container/window_drop_down
 
 var resolutions : Dictionary = {
 	"800x600":Vector2i(800, 600),
@@ -11,10 +12,8 @@ var resolutions : Dictionary = {
 }
 
 var window_modes : Dictionary = {
-	"Windowed":0,
-	"Borderless-Window":1,
-	"Borderless":2,
-	"Fullscreen":3
+	"Window":DisplayServer.WINDOW_MODE_WINDOWED,
+	"Fullscreen":DisplayServer.WINDOW_MODE_FULLSCREEN
 }
 
 func _ready() -> void:
@@ -22,8 +21,9 @@ func _ready() -> void:
 	AudioServer.add_bus()
 	AudioServer.set_bus_name(1, "Music")
 	AudioServer.set_bus_name(2, "SFX")
-	add_resolutions_to_option_btn()
+	add_resolutions_to_option_btns()
 	res_drop_down.selected=1
+	window_drop_down.selected=0
 
 func slider_to_db(value: float) -> float:
 	return lerpf(-20, 0, value / 100.0)
@@ -39,15 +39,25 @@ func _on_audio_slider_value_changed(value: float, channel: StringName) -> void:
 func _on_save_pressed() -> void:
 	game.menu.back()
 
-func add_resolutions_to_option_btn():
+func add_resolutions_to_option_btns():
 	for r in resolutions:
 		res_drop_down.add_item(r)
+	for m in window_modes:
+		window_drop_down.add_item(m)
 
 func _on_res_drop_down_item_selected(index: int) -> void:
-	var key=res_drop_down.get_item_text(index)
+	var key = res_drop_down.get_item_text(index)
 	var size = resolutions.get(key)
 	get_window().set_size(size)
 	
 	var screen_center = DisplayServer.screen_get_position()+DisplayServer.screen_get_size()/2
 	var window_size = get_window().get_size_with_decorations()
 	get_window().set_position(screen_center-window_size/2)
+
+
+func _on_border_check_toggled(toggled_on: bool) -> void:
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,toggled_on)
+
+
+func _on_window_drop_down_item_selected(index: int) -> void:
+	DisplayServer.window_set_mode(window_modes.get(window_drop_down.get_item_text(index)))
