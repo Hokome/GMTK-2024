85b83fd22a2137ffc393479a846b25ae67e6439b
Hokome <m.k.esseghir@protonmail.com>
2024-08-19 23:57:51 +0200

    Piano musician

--- scripts/attack/ally_hitbox.gd
+++ scripts/attack/ally_hitbox.gd
@@ -1,12 +1,16 @@
 extends Area2D
 class_name AllyHitbox
 
+enum DebuffType {NONE, ATTACK, SPEED}
+
 @export var damage: float
+@export var effect_type: DebuffType
 
 var hit_bodies: Array[Node2D] = []
 var colliding_bodies: Array[Node2D]
 
 func _ready() -> void:
+	if effect_type == DebuffType.ATTACK: print("yes")
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
@@ -15,7 +19,13 @@ func _on_body_entered(body: Node2D) -> void:
 	if hit_bodies.has(body): return
 	
 	colliding_bodies.append(body)
-	body.get_node("health").damage(damage)
+	match effect_type:
+		DebuffType.NONE:
+			body.get_node("health").damage(damage)
+		DebuffType.ATTACK:
+			body.get_node("hitbox").damage_debuff_time = damage
+		DebuffType.SPEED:
+			body.speed_debuff_time = damage
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is not Entity: return
