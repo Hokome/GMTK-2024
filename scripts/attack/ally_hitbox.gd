7c07e2bb9688f7cf1221a9b2353dedcfa3e52fd0
Hokome <m.k.esseghir@protonmail.com>
2024-08-19 14:04:14 +0200

    Enemy scaling and tuba fix

--- scripts/attack/ally_hitbox.gd
+++ scripts/attack/ally_hitbox.gd
@@ -4,9 +4,11 @@ class_name AllyHitbox
 @export var damage: float
 
 var hit_bodies: Array[Node2D] = []
+var colliding_bodies: Array[Node2D]
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is not Entity: return
@@ -15,5 +17,12 @@ func _on_body_entered(body: Node2D) -> void:
 	hit_bodies.append(body)
 	body.get_node("health").damage(damage)
 
+func _on_body_exited(body: Node2D) -> void:
+	if body is not Entity: return
+	if colliding_bodies.has(body):
+		colliding_bodies.erase(body)
+
 func reset_hit():
 	hit_bodies.clear()
+	for b in colliding_bodies:
+		b.get_node("health").damage(damage)
--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -3,10 +3,7 @@ class_name Spawner
 
 var spawn = preload("res://scenes/enemy.tscn")
 var offset: float = 500
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+const HP_PER_SECOND: float = 0.5
 
 func _on_timer_timeout() -> void:
 	var enemy: Entity = spawn.instantiate()
@@ -15,3 +12,6 @@ func _on_timer_timeout() -> void:
 	enemy.global_position = global_position + random_direction * offset
 	
 	enemy.get_node("follow_player").target = game.player
+	var enemy_health: Health = enemy.get_node("health")
+	enemy_health.max_health += HP_PER_SECOND / game.elapsed_time
+	enemy_health.health = enemy_health.max_health
