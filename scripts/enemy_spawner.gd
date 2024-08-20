58d5da2e04aca13fccf28c287bf2342d6b4aa2ca
Raylock <maruthikasyapaditya@gmail.com>
2024-08-20 16:05:12 +0530

    broken_enemy spawning

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -14,9 +14,20 @@ func _on_timer_timeout() -> void:
 	var enemy: Entity = spawn.instantiate()
 	game.add_child(enemy)
 	
-	var random_direction= Vector2(randf() - 0.5, randf() - 0.5).normalized()
+	var spawn_loc
+	var counter = 10
+	while counter>0:
+		counter-=1
+		var random_direction= Vector2(randf() - 0.5, randf() - 0.5).normalized()
+		#spawn_loc = global_position + random_direction * offset
+		spawn_loc = get_global_mouse_position()
+		var tile = tilemap_layer.get_cell_tile_data(tilemap_layer.local_to_map(spawn_loc))
+		if !tile || !tile.get_custom_data("spawnable"):	
+			print(tilemap_layer.local_to_map(spawn_loc),tile)
+			enemy.queue_free()
+			return
 	
-	enemy.global_position = global_position + random_direction * offset
+	enemy.global_position = spawn_loc
 	
 	enemy.get_node("follow_player").target = game.player
 	var enemy_health: Health = enemy.get_node("health")
